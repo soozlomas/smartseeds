7bc727da2653988c85edf25f446e35068eb740a2
soozlomas <sooz@Soozs-MacBook-Pro.local>
2015-10-10 11:39:30 +1100

    added image flip

--- js/script.js
+++ js/script.js
@@ -8,4 +8,27 @@ $(document).ready(function(){
       $('#mobilenav').toggle("slow");
   });
 
+  $(function () {
+  if ( $('html').hasClass('csstransforms3d') ) {
+    $('.artGroup').removeClass('slide').addClass('flip');
+    $('.artGroup.flip').on('mouseenter',
+      function () {
+        $(this).find('.artwork').addClass('theFlip');
+      });
+    $('.artGroup.flip').on('mouseleave',
+      function () {
+        $(this).find('.artwork').removeClass('theFlip');
+      });
+  } else {
+    $('.artGroup').on('mouseenter',
+      function () {
+        $(this).find('.detail').stop().animate({bottom:0}, 500, 'easeOutCubic');
+      });
+    $('.artGroup').on('mouseleave',
+      function () {
+        $(this).find('.detail').stop().animate({bottom: ($(this).height() + -1) }, 500, 'easeOutCubic');
+      });
+  }
+  });
+
 });
